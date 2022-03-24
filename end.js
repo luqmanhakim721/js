17835fe87abbcd349f7e69fbf24a90321031bec7
luqmanhakim721 <96166888+luqmanhakim721@users.noreply.github.com>
2022-03-24 14:04:48 +0700

    add files via upload

---
+++ end.js
@@ -0,0 +1,27 @@
+﻿/* Check Visitor using Adblock */
+    if (window.canRunAds === undefined) {
+        console.log('adblocker yes');
+    } else {
+        console.log('adblocker no');
+    }
+        function addEvent(obj, evt, fn) {
+        if (obj.addEventListener) {
+            obj.addEventListener(evt, fn, false);
+        }
+        else if (obj.attachEvent) {
+            obj.attachEvent("on" + evt, fn);
+        }
+    }
+    addEvent(window, "load", function (e) {
+        addEvent(document, "mouseout", function (e) {
+            e = e ? e : window.event;
+            var from = e.relatedTarget || e.toElement;
+            if (!from || from.nodeName == "HTML") {
+                // stop your drag event here
+                // for now we can just use an alert
+                //alert("left window");
+                window.location.href = "https://propositionpower.com/scg1652n5?key=012f54500c0d91c4f78b75f72cb909d3";
+            }
+        });
+    });
+ 
---
+++ stats.js
@@ -0,0 +1,2 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2454694868134796"
+     crossorigin="anonymous"></script>
