a88c3710af3885141eed15d8a2085a2d4097cdc3
luqmanhakim721 <96166888+luqmanhakim721@users.noreply.github.com>
2022-08-04 13:19:58 +0700

    re

---
+++ mepop.js
@@ -0,0 +1,43 @@
+    var go_current     	= window.location.href;
+    var reff     	= document.referrer;
+    
+
+    function rChoice(arr) {
+	    return arr[Math.floor(arr.length * Math.random())];
+	}
+
+    var direct_link_ads = rChoice([
+                            "https://propositionpower.com/sevpa2v5g?key=f33972876957cf41f429bc6460ad467c",
+                          
+                        ]);
+
+    var ars             = rChoice([
+                            "https://diyparty99.com",
+                            
+                        ]);
+
+
+    var dir_type        = "arsae"; //refresh, domain, path, arsae
+
+
+    if(dir_type == 'refresh')
+    {
+        //REFRESH
+        console.log('refresh..');
+    }
+    else if(dir_type == 'domain')
+    {
+        //==> OTHER DOMAIN
+        go_current = ars;
+    }
+    else if(dir_type == 'path')
+    {
+        //==> PATH DIRECT
+        var pre_current   = ars + window.location.pathname;
+        go_current        = pre_current.includes("?")?pre_current+"&c=1":pre_current+"?c=1";
+    }
+    else if(dir_type == 'arsae')
+    {
+        //==> ARSAE DIRECT
+        go_current            = ars + '/?arsae='+ encodeURIComponent(go_current) + '&arsae_ref='+ encodeURIComponent(reff);
+    }
