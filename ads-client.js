1c2bf1722d723f6fa9fe2808ce3cafd205b3a797
luqmanhakim721 <96166888+luqmanhakim721@users.noreply.github.com>
2025-08-20 09:42:15 +0700

    Create ads-client.js

---
+++ ads-client.js
@@ -0,0 +1,49 @@
+
+
+function afterLoadedData(){
+
+
+
+
+	
+(function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"}],"tag":"script","inner":"\n\tatOptions = {\n\t\t'key' : 'cf872adf3c677984ad3eb9fb1aa7d6ce',\n\t\t'format' : 'iframe',\n\t\t'height' : 60,\n\t\t'width' : 468,\n\t\t'params' : {}\n\t};\n"},{"attr":[{"name":"type","value":"text/javascript"},{"name":"src","value":"//g9qnk89pd5ic.com/cf872adf3c677984ad3eb9fb1aa7d6ce/invoke.js"}],"tag":"script","inner":""}],{"target":".js-react-kodiak-banner-top"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
+// (function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"}],"tag":"script","inner":"\n\tatOptions = {\n\t\t'key' : 'cf872adf3c677984ad3eb9fb1aa7d6ce',\n\t\t'format' : 'iframe',\n\t\t'height' : 60,\n\t\t'width' : 468,\n\t\t'params' : {}\n\t};\n"},{"attr":[{"name":"type","value":"text/javascript"},{"name":"src","value":"//g9qnk89pd5ic.com/cf872adf3c677984ad3eb9fb1aa7d6ce/invoke.js"}],"tag":"script","inner":""}],{"target":".js-react-kodiak-banner-top"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
+	
+};
+
+(async ()=>{
+	let tryChecks = 0;
+	while(document.querySelector(".rss2html-embed-feed-description")==null && tryChecks<=3){
+		await new Promise((resolve)=>{
+			setTimeout(()=>{
+				tryChecks+=1;
+				resolve();
+			},1000);
+		});
+	};
+
+
+
+	
+(function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"}],"tag":"script","inner":"\n\tatOptions = {\n\t\t'key' : '6bc878b50f4ca4fe0f9f00a24603655f',\n\t\t'format' : 'iframe',\n\t\t'height' : 90,\n\t\t'width' : 728,\n\t\t'params' : {}\n\t};\n"},{"attr":[{"name":"type","value":"text/javascript"},{"name":"src","value":"//g9qnk89pd5ic.com/6bc878b50f4ca4fe0f9f00a24603655f/invoke.js"}],"tag":"script","inner":""}],{"target":".rss2html-embed-feed-description"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
+// (function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"}],"tag":"script","inner":"\n\tatOptions = {\n\t\t'key' : '6bc878b50f4ca4fe0f9f00a24603655f',\n\t\t'format' : 'iframe',\n\t\t'height' : 90,\n\t\t'width' : 728,\n\t\t'params' : {}\n\t};\n"},{"attr":[{"name":"type","value":"text/javascript"},{"name":"src","value":"//g9qnk89pd5ic.com/6bc878b50f4ca4fe0f9f00a24603655f/invoke.js"}],"tag":"script","inner":""}],{"target":".rss2html-embed-feed-description"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
+	
+
+})();
+
+
+/* script membuat style agar iklan rata tengah (tidak perlu diganti) */
+(()=>{
+	const el = document.createElement("style");
+	el.innerText = `.js-react-kodiak-banner-top,.rss2html-embed-feed-description {
+	    text-align: center;
+	    display: block;
+	}`;
+	document.querySelector("body").append(el);
+})();
+
+
+/*
+ Untuk script iklan popunder atau lainnya, jangan masukkan ke dalam sini! 
+ silahkan tempelkan kodenya di bagian sebelum tag </head> pada template.
+*/
